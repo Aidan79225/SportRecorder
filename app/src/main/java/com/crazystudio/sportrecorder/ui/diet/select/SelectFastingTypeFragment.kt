5e4fe1b86d6c1630bb530fbb1b218e1fe9aeab6d
Aidan Wang <aidan79225@gmail.com>
2021-08-11 03:00:55 +0800

    Update build.gradle, SelectFastingTypeFragment.kt, and mobile_navigation.xml

--- app/src/main/java/com/crazystudio/sportrecorder/ui/diet/select/SelectFastingTypeFragment.kt
+++ app/src/main/java/com/crazystudio/sportrecorder/ui/diet/select/SelectFastingTypeFragment.kt
@@ -1,7 +1,10 @@
 package com.crazystudio.sportrecorder.ui.diet.select
 
+import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
@@ -11,18 +14,26 @@ import com.crazystudio.sportrecorder.ui.base.BaseFragment
 import com.crazystudio.sportrecorder.util.Constants
 import com.crazystudio.sportrecorder.util.DietPreference
 import com.crazystudio.sportrecorder.util.dpToPx
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class SelectFastingTypeFragment : BaseFragment(R.layout.fragment_select_fasting_type) {
+class SelectFastingTypeFragment : BottomSheetDialogFragment() {
     private val viewModel by viewModels<SelectFastingTypeViewModel>()
     @Inject lateinit var dietPreference: DietPreference
+    lateinit var binding: FragmentSelectFastingTypeBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return FragmentSelectFastingTypeBinding.inflate(inflater, container, false).apply {
+            binding = this
+        }.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        FragmentSelectFastingTypeBinding.bind(view).apply {
-
+        binding.apply {
             val selectFastingTypeAdapter = SelectFastingTypeAdapter(
                 object : SelectFastingTypeAdapter.FastingItemClickListener {
                     override fun onClick(fastingHours: Long, eatingHours: Long) {
@@ -50,6 +61,16 @@ class SelectFastingTypeFragment : BaseFragment(R.layout.fragment_select_fasting_
                 }
                 adapter = selectFastingTypeAdapter
                 addItemDecoration(SpaceItemDecoration(context.dpToPx(10f).toInt(), 2))
+            }
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            setOnShowListener {
+                (it as? BottomSheetDialog)?.run {
+                    behavior.state = BottomSheetBehavior.STATE_EXPANDED
+                }
             }
         }
     }
